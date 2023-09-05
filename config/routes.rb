1be748994c000559a97379ff580f264a182839ac
Stefanos Soulis <stefsoulis@gmail.com>
2023-09-05 10:16:26 +0200

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   devise_for :users
-  # root to: "pages#home"
+  root to: "pages#home"
 
   # authenticated :user do
   #   root to: "users#show"
