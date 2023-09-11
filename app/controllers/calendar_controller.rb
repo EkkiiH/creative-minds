dc2d3bfab3234f767b534c948e2e347d2d9c7fce
Stefanos Soulis <stefsoulis@gmail.com>
2023-09-11 17:20:14 +0300

    calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,11 +6,5 @@ class CalendarController < ApplicationController
 
   def index
     @tasks = policy_scope(Task)
-    @tasks = Task.where(
-      due_date: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week
-    )
   end
-  @subtasks = Subtask.where(
-    due_date: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week
-  )
 end
