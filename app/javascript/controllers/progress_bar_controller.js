5604a27fa26b475895de3d9b8829999362cc0273
Olivia Wong <oliviawong@outlook.com>
2023-09-11 17:49:08 +0200

    progress bar done

--- app/javascript/controllers/progress_bar_controller.js
+++ app/javascript/controllers/progress_bar_controller.js
@@ -15,4 +15,9 @@ export default class extends Controller {
     this.progressTarget.style.background = `radial-gradient(closest-side, white 79%, transparent 80% 100%),conic-gradient(#03A9F4, #AB47BC, #F59652 ${percentage}%, #BBB 0)`
     this.progressTarget.setAttribute("percentage", `${percentage}%`)
   }
+
+  toggleTodo() {
+    console.log(this.completedplansValue)
+    console.log(this.plansValue)
+  }
 }
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,17 @@ class Task < ApplicationRecord
   validates :activity, presence: true
   validates :priority, numericality: { in: 0..2 }
 
+  # When a task is updated, then lets reoplace the progress bar
+  after_update_commit :update_progress_bar
+
   def start_time
     self.due_date
+  end
+
+  def update_progress_bar
+    broadcast_replace_to plan,
+      target: "plan_#{plan.id}_progress",
+      partial: 'plans/progress',
+      locals: { plan: plan }
   end
 end
