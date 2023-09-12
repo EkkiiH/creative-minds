40061d94c56d75344359ae198e174618593d0d25
Stefanos Soulis <stefsoulis@gmail.com>
2023-09-12 23:31:28 +0300

    task-alert

--- app/javascript/controllers/popup_controller.js
+++ app/javascript/controllers/popup_controller.js
@@ -2,20 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="popup"
 export default class extends Controller {
+  static targets=["hiddenField"]
   connect() {
-  console.log("hello")
   }
 create(event){
   event.preventDefault()
   const confirmation = confirm('Do you want to add a task for this plan?');
   if (confirmation) {
-    const form = new FormData(this.element)
-    form.append("new_task", "true")
-    form.submit()
+    this.hiddenFieldTarget.value = "true";
+    this.element.submit();
   } else {
     // Submit the plan form
-    this.submit();
+    this.element.submit();
   }
- console.log("hi")
 }
 }
